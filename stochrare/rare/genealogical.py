3bc4438141f728e07e4b48fef9c48080acbabfbc
AlessandroLovo <ale571.lovo@gmail.com>
2022-09-09 15:56:48 +0200

    Starting to properly implement the algorithm

--- stochrare/rare/genealogical.py
+++ stochrare/rare/genealogical.py
@@ -5,25 +5,68 @@ Examples are the interactive particle system, the GKTL algorithm or Garnier del 
 """
 # TODO: add references
 
+import numpy as np
+
 class Base():
 
-    def __init__(self, scorefun, ensemble_size=10):
+    def __init__(self, scorefun, ensemble_size=10, timestep=None, k=1):
         self.scorefun = scorefun
         self.ensemble_size = ensemble_size
+        self.timestep = timestep
+        self.k = k
+
         self._ensemble = None
+        self._weights = None
+        self._norm_factor = None
 
     def initialize_ensemble(self):
-        pass
+        raise NotImplementedError('This is the base class you fool!')
 
-    def select(self):
-        pass
+    def prepare_for_next_step(self, ensemble_ids):
+        new_ensemble = []
+        for j in ensemble_ids:
+            new_ensemble.append(self._ensemble[j].copy())
+        self._ensemble = new_ensemble
 
-    def propagate(self):
-        pass
+    def select(self) -> list[int]:
+        '''
+        Selects ensemble members that survive to the next generation (with repetitions) according to `self._weights`
+        Returns a list of size `self.ensemble_size` that contains integer values between `0` and `self.ensemble_size - 1`
+        '''
+        return np.random.choice(self.ensemble_size,size=self.ensemble_size,p=self._weights)
 
-    def run(self, n_iter):
+    def propagate_ensemble(self):
+        '''
+        Propagates all the ensemble members forward in time for `self.timestep`
+        '''
+        for e in self._ensemble:
+            e.update(self.timestep)
+
+    def compute_weights(self):
+        self._norm_factor = None
+        self._weights = np.array([e.compute_weight(self.scorefun) for e in self._ensemble])
+
+    def normalize_weights(self):
+        if self._norm_factor is not None:
+            raise ValueError('You are trying to normalize the weights twice')
+        self._weights = np.exp(self.k*self._weights)
+        self._norm_factor = np.sum(self._weights)
+        self._weights /= self._norm_factor
+
+    def step(self):
+        '''
+        Performs one step of the algorithm
+        '''
+        self.propagate_ensemble()
+        self.compute_weights()
+        self.normalize_weights()
+        survivors = self.select()
+        self.prepare_for_next_step(survivors)
+
+
+    def run(self, n_iter: int):
         if self._ensemble is None:
             self.initialize_ensemble()
+
         for i in range(n_iter):
-            self.select()
-            self.propagate()
+            self.step()
